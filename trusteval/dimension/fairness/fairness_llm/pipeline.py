e57524dfbbace4d930bb8d6b218cbd9df093bd08
nauyisu022 <59754221+nauyisu022@users.noreply.github.com>
2025-04-06 01:46:08 +0800

    fix several bugs

--- trusteval/dimension/fairness/fairness_llm/pipeline.py
+++ trusteval/dimension/fairness/fairness_llm/pipeline.py
@@ -7,20 +7,23 @@ from .disparagement import DisparagementGenerator
 
 
 
-def run(base_dir=None):
-    print("Running StereotypeGenerator ...")
+def run(base_dir=None, subset=['stereotype', 'preference', 'disparagement']):
+    if 'stereotype' in subset:
+        print("Running StereotypeGenerator ...")
+        stereotype_generator = StereotypeGenerator(base_dir)
+        stereotype_generator.run()
     
-    stereotype_generator=StereotypeGenerator(base_dir)
-    stereotype_generator.run()
+    if 'preference' in subset:
+        print("Running PreferenceGenerator ...")
+        preference_generator = PreferenceGenerator(base_dir)
+        preference_generator.run()
     
-    # print("Running PreferenceGenerator ...")
-    # preference_generator=PreferenceGenerator(base_dir)
-    # preference_generator.run()
+    if 'disparagement' in subset:
+        print("Running DisparagementGenerator ...")
+        disparagement_generator = DisparagementGenerator(base_dir)
+        disparagement_generator.run()
     
-    # print("Running DisparagementGenerator ...")
-    # disparagement_generator=DisparagementGenerator(base_dir)
-    # disparagement_generator.run()
-    # print("All dataset generation finished.",color="GREEN")
+    print("All dataset generation finished.", color="GREEN")
 
     
         
